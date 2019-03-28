39820678e4ce1482070747e9bb15741fe5b0d0a0
Azat Akhunov <azat.akhunov@protonmail.com>
2019-03-28 22:08:08 +0300

    [NF] token validation controller mapper injection

--- src/main/java/com/botmasterzzz/auth/controller/AuthController.java
+++ src/main/java/com/botmasterzzz/auth/controller/AuthController.java
@@ -16,9 +16,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
@@ -79,4 +77,15 @@ public class AuthController {
         return ResponseEntity.created(location)
                 .body(new ApiResponse(true, "User registered successfully@"));
     }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/check")
+    public ApiResponse tokenCheck(@Valid @RequestParam String token) {
+        boolean validation = tokenProvider.validateToken(token);
+        if (validation){
+            return new ApiResponse(validation, "good");
+        }else{
+            return new ApiResponse(validation, "bad");
+        }
+    }
+
 }
