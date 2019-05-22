61a2ba5a199690fd9b994c9474638747d72621c8
Azat Akhunov <azat.akhunov@protonmail.com>
2019-05-22 11:54:31 +0300

    [NF] new liquibase file added - new column added

--- src/main/java/com/botmasterzzz/auth/controller/UserController.java
+++ src/main/java/com/botmasterzzz/auth/controller/UserController.java
@@ -6,7 +6,6 @@ import com.botmasterzzz.auth.repository.UserRepository;
 import com.botmasterzzz.auth.security.CurrentUser;
 import com.botmasterzzz.auth.security.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
