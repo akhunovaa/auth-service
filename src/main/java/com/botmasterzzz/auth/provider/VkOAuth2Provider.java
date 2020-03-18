e1442b3fa0d2cde185314768e0fd64698746a6fb
Azat Akhunov <azat.akhunov@protonmail.com>
2020-03-19 02:02:41 +0300

    [NF] vk oauth2 authorization added

--- src/main/java/com/botmasterzzz/auth/provider/VkOAuth2Provider.java
+++ src/main/java/com/botmasterzzz/auth/provider/VkOAuth2Provider.java
@@ -10,7 +10,7 @@ public enum VkOAuth2Provider {
         public ClientRegistration.Builder getBuilder(String registrationId) {
             ClientRegistration.Builder builder = this.getBuilder(registrationId, ClientAuthenticationMethod.POST, "{baseUrl}/{action}/oauth2/code/{registrationId}");
             builder.scope(new String[]{"email", "photos"});
-            builder.authorizationUri("https://oauth.vk.com/authorize?v=5.95");
+            builder.authorizationUri("https://oauth.vk.com/authorize?v=5.95&display=popup");
             builder.tokenUri("https://oauth.vk.com/access_token");
             builder.userInfoUri("https://api.vk.com/method/users.get?{user_id}&{access_token}&v=5.8&fields=photo_id, verified, sex, bdate, city, country, home_town, has_photo, photo_50, photo_100, photo_200_orig, photo_200, photo_400_orig, photo_max, photo_max_orig, online, domain, has_mobile, contacts, site, education, universities, schools, status, last_seen, followers_count, common_count, occupation, nickname, relatives, relation, personal, connections, exports, activities, interests, music, movies, tv, books, games, about, quotes, can_post, can_see_all_posts, can_see_audio, can_write_private_message, can_send_friend_request, is_favorite, is_hidden_from_feed, timezone, screen_name, maiden_name, crop_photo, is_friend, friend_status, career, military, blacklisted, blacklisted_by_me, can_be_invited_group");
             builder.userNameAttributeName("id");
