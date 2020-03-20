503255a9a77a0fc110aa5b0b4a00c39f73c7b6fe
Azat Akhunov <azat.akhunov@protonmail.com>
2020-03-20 15:50:56 +0300

    [NF] cors added for dev and local

--- src/main/java/com/botmasterzzz/auth/filter/TokenAuthenticationFilter.java
+++ src/main/java/com/botmasterzzz/auth/filter/TokenAuthenticationFilter.java
@@ -56,7 +56,6 @@ public class TokenAuthenticationFilter extends GenericFilterBean {
         }
         if (allowOrigin) {
             HttpServletResponse res = (HttpServletResponse) response;
-            res.setHeader("Access-Control-Allow-Origin", "*");
             res.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
             res.setHeader("Access-Control-Max-Age", "3600");
             res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
